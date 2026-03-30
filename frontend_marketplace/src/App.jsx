5b7d83a12bd748a20703dde4b5b3d81f443219ef
SOHAM <24155127@kiit.ac.in>
2026-03-30 16:31:38 +0530

    commit

--- frontend_marketplace/src/App.jsx
+++ frontend_marketplace/src/App.jsx
@@ -1,19 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import ProductGrid from "./components/ProductGrid";
 import Footer from "./components/Footer";
-import Categories from "./components/Categories";
+
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Hero />
-      <Categories />
-      <ProductGrid />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
